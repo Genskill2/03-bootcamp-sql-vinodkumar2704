999546dd6ab6b15e257d5d05c534be2eec6222a8
vinodkumar2704 <vinodkb2704@gmail.com>
2021-06-16 15:21:36 +0530

    updated query3

--- query3.sql
+++ query3.sql
@@ -1,6 +1,6 @@
 SELECT books.title 
 FROM books,subjects,books_subjects
 WHERE subjects.name in ('Technology','Politics')
-AND books_subjects.subject = subject.id
+AND books_subjects.subject = subjects.id
 AND books_subjects.book = books.id;
 
